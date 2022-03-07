818520760e07df9748ff4174729805b5aa5ad282
Roman Ptashnyk <2508romaa@gmail.com>
2022-03-07 16:37:33 +0200

    ReactJS-25

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -21,7 +21,7 @@ const Message = (props) => {
 };
 
 function Messages(props) {
-  let dialogsData = [
+  let dialogs = [
     { name: 'Dimych', id: 1 },
     { name: 'Andrey', id: 2 },
     { name: 'Viktor', id: 3 },
@@ -29,28 +29,26 @@ function Messages(props) {
     { name: 'Dmitry', id: 5 },
   ];
 
-  let messagesData = [
+  let messages = [
     { message: 'Hi', id: 1 },
     { message: 'How are you', id: 2 },
     { message: 'Yo', id: 3 },
   ];
 
+  let dialogsElements = dialogs.map((d) => (
+    <DialogItem name={d.name} id={d.id} />
+  ));
+
+  let messagesElements = messages.map((m) => (
+    <Message message={m.message} id={m.id} />
+  ));
+
   return (
     <div className={classes.dialogs}>
       <h2 className={classes.messages__title}>DIALOGS</h2>
       <div className={classes.messages__wrapper}>
-        <div className={classes.messages__dialogs}>
-          <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-          <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-          <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-          <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-          <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-        </div>
-        <div className={classes.messages__content}>
-          <Message message={messagesData[0].message} />
-          <Message message={messagesData[1].message} />
-          <Message message={messagesData[2].message} />
-        </div>
+        <div className={classes.messages__dialogs}>{dialogsElements}</div>
+        <div className={classes.messages__content}>{messagesElements}</div>
       </div>
     </div>
   );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,11 +3,12 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 function MyPosts() {
+  let posts = [
+    { id: 1, message: 'Hello, how are you?', likesCount: '15' },
+    { id: 2, message: "It's my first project.", likesCount: '20' },
+  ];
 
-  let postsData = [
-    {id: 1, message: "Hello, how are you?", likesCount: "15"},
-    {id: 2, message: "It's my first project.", likesCount: "20"},
-  ]
+  let postsElements = posts.map((p) => <Post message={p.message} likesCount={p.likesCount} />);
 
   return (
     <div className={classes.myPosts}>
@@ -21,10 +22,7 @@ function MyPosts() {
             <button>Send</button>
           </div>
         </div>
-        <div>
-          <Post message={postsData[0].message} likesCount={postsData[0].likesCount} />
-          <Post message={postsData[1].message} likesCount={postsData[1].likesCount} />
-        </div>
+        <div>{postsElements}</div>
       </div>
     </div>
   );
